e06886e512983c2868347c74e39e8febdfb3a848
Mario Morales <mario.morales@devsu.com>
2021-10-04 11:10:53 -0500

    added form with validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-forms-practice-td';
+
+   onSubmit(form: NgForm) {
+      console.log(form);
+   }
 }
